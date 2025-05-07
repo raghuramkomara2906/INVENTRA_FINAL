37b92f2c7fb2644c835d43b5ccfbfc60450e1abb
raghuramkomara2906 <raghuramkomara@usf.edu>
2025-05-06 23:36:11 -0400

    comit

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADAProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET /About or /About/Index
+        public IActionResult Index()
+        {
+            return View();    
+        }
+    }
+}
