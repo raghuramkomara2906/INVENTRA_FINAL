85722ce9f6e1dce703d2613304cbc95f0b365f69
raghuramkomara2906 <raghuramkomara@usf.edu>
2025-05-06 23:42:21 -0400

    sample

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ADAProject.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Models/User.cs
+++
@@ -1,8 +0,0 @@
-public class User
-{
-  public int Id { get; set; }
-  public string UserName { get; set; } = null!;
-  public string PasswordHash { get; set; } = null!;
-
-  public string Email { get; set; } = null!; 
-}
